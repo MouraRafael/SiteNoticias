a2f14a30ed3959bde678f24fc3f20aac2f7f4bc6
Rafael Moura <rafael.g.moura@hotmail.com>
2022-05-11 15:01:08 -0300

    update: mudando metodo send para render

--- app.js
+++ app.js
@@ -16,36 +16,13 @@ app.set('views','./app/views');
 
 // rota home
 app.get('/',function(req,res){
-    res.send(
-        `<html> 
-            <head>
-                <meta charset="UTF-8">
-                <title> Portal de Notícias</title>
-                <body>
-                    <a href="/noticias">Notícias</a>
-                    <h1>Portal de Notícias</h1>
-                <body>
-            </head> 
-        </html>`
-    )
+    // o O EJS disponibiliza o método render para usar nas respostas das requi
+    res.render('home/index'); 
 });
 
 
 app.get('/noticias',function(req,res){
-    res.send(`<html> 
-    <head>
-        <meta charset="UTF-8">
-        <title> Portal de Notícias</title>
-        <body>
-            <a href="/">Home</a>
-            <h1>Todas as Notícias</h1>
-            <ul>
-                <li><a href="#">Servidor NODE.JS com Express</li>    
-                <li><a href="#">NODEMON melhora a performance do desenvolvedor NODE.JS</li>    
-            </ul>
-        <body>
-    </head> 
-</html>`)
+    res.render('noticias/noticias')
 })
 
 app.listen(3000,()=>{
