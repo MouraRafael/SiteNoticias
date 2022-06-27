95d3beb7d118e5f47d6b91224e30027ee7899227
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-27 16:44:49 -0300

    feat: passando a rota para a noticia

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.get('/',function(req,res){
 app.get('/noticias',function(req,res){
     res.render('noticias/noticias',{noticias: noticias}); //além do arquivo ejs ('noticias/noticias'), será passada uma variável com o conteúdo do arquivo mockup para que possamos trabalhar com esses valores na página
 })
-
+//Rota Notícia
+app.get('/noticia',function(req,res){
+    // recupera id noticia por get
+    const id = req.query.id
+    res.render('noticias/noticia',{noticia: noticias[id]})
+})
 //Rota Admin, adicionar Notícias
 app.get('/admin',function(req,res){
     res.render('admin/form_add_noticias')
