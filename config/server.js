4514dc72bf1ccacc05c8390fdea9d8db5f8ccee5
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-26 22:58:22 -0300

    feat: express.static

--- config/server.js
+++ config/server.js
@@ -8,4 +8,7 @@ app.set('view engine', 'ejs');
 // Configurar o caminho da pasta views
 app.set('views','./app/views');
 
+// configuação dos arquivos estáticos
+app.use(express.static('./app/public'))
+
 module.exports = app
